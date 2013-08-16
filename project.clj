9b6cc9f63080939d31f1366d5264de27af1e5721
KingCode <jrompre@gmail.com>
2013-08-15 21:07:28 -0400

    Made it a snapshot version

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject clj_utils "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject org.clojars.kinguscodus/clj_utils "0.1.0-SNAPSHOT"
+  :description "Clojure utilities"
+  :url "https://github.com/kingcode/clj_utils"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :warn-on-reflection true
   :dependencies [[org.clojure/clojure "1.5.1"]
                  ;;[clojurecheck "2.0.2"]
                  [marginalia "0.7.1"]
-                 [org.clojure/core.memoize "0.5.6"]
-                 [org.clojure/core.rrb-vector "0.0.9"]
+                 ;;[org.clojure/core.memoize "0.5.6"]
+                 ;;[org.clojure/core.rrb-vector "0.0.9"]
                  [commons-io/commons-io "2.4"]])
