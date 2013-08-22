8c5a1722d3bd40007feeb0f7dae2da3dbaf5128a
KingCode <jrompre@gmail.com>
2013-08-22 11:45:20 -0400

    Added to core is-case, macro reflection fns

--- src/clj_utils/core.clj
+++ src/clj_utils/core.clj
@@ -1,4 +1,42 @@
-(ns clj-utils.core)
+(ns clj-utils.core
+    (:use clojure.walk))
+
+
+(defn funcvar
+"Yields the var for a macro or function symbol.
+"
+[ sym ]
+  (->> sym str (str "#'") read-string eval))
+
+
+
+(defn macro? 
+"Yields true if sym points to a var referencing a macro, false otherwise.
+"
+[ sym ]
+  (-> sym funcvar meta :macro true?))
+
+
+(defn macro-call
+"Yields a form which invokes a macro function with (rest form) as arguments.
+"
+([ form ]        
+  (let [ sym (first form) 
+         args (->> (rest form)
+                   (walk #(do `(quote ~%)) identity)) ]
+    #_(println "REST FORM: " (rest form) ", ARGS: " args)
+    `(eval (@(funcvar '~sym) ~form {} ~@args))))
+([ sym args ]
+    `(eval (@(funcvar '~sym) nil {} ~@args))))
+
+
+(defn is-case
+"Yields the result of (pred expr). A one-arg to two-args predicate adapter intended for
+ use with condp.
+"
+[ pred expr ] (true? (pred expr)))
+
+
 
 ;; dummy def for rebinding within thread-it 
 (def it "for use by thread-it")
@@ -15,6 +53,36 @@
   (if (empty? rest-expr) first-expr
             `(let [~'it ~first-expr]
                 (thread-it ~@rest-expr))))
+
+
+(defn preserve-metadata
+"Ensures that the body containing `expr` will carry the metadata
+from `&form`. Derived from Clojure Programming, by Emerick/Carper/Grand
+"
+[&form expr]
+  (let [res (with-meta (gensym "res") (meta &form))]
+      `(let [~res ~expr]
+               ~res)))
+
+
+(defmacro defmacro-withmeta
+"Same as `clojure.core/defmacro`, but preserves user-supplied metadata
+(e.g. type hints).Derived from Clojure Programming, by Emerick/Carper/Grand.
+"
+([ macrocall ]
+  (preserve-metadata &form `((fn [] ~macrocall)))))
+
+
+#_([name doc-string? attr-map? ([params*] body) + attr-map?])
+#_(
+([] nil)
+([x] (preserve-metadata &form x))
+([x & next]
+    (preserve-metadata 
+                 &form 
+                `(let [or# ~x]
+                    (if or# or# (or ~@next))))))
+
 
 
 (defn str2int
--- test/clj_utils/core_test.clj
+++ test/clj_utils/core_test.clj
@@ -1,7 +1,21 @@
 (ns clj-utils.core-test
-  (:use clojure.test
-        clj-utils.core))
+  (:use clojure.test)
+  (:use clj-utils.core))
 
-#_(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+(deftest macro-call-test
+  (testing "Should yield a eval-ready form from a for with a macro in function position"
+     (is (= 1 (eval (macro-call '(let [x 0] (inc x))))))
+
+     ;;NOTE: using a regular quote here, and so have to fully specify -> is-case 
+     (let [ form '(let [x "x"] (condp clj-utils.core/is-case x #(not (number? %)) "not a number" odd? "odd" even? "even")) ]
+        (is (= "not a number" (eval (macro-call form)))))
+
+     ;;NOTE: using syntax-quote
+     (let [ form `(let [~'x 1] (condp is-case ~'x #(not (number? %)) "not a number" odd? "odd" even? "even")) ]
+        (is (= "odd" (eval (macro-call form)))))
+
+     (let [ form `(let [~'x 0] (condp is-case ~'x #(not (number? %)) "not a number" odd? "odd" even? "even")) ]
+        (is (= "even" (eval (macro-call form)))))
+))
+
